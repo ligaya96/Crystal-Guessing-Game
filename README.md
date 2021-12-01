217123de0118af57882c64334b76a223b6f69318
Ligaya <ligaya96@gmail.com>
2021-11-30 18:49:40 -0800

    added some JS

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Word-Guess-Game
 a quick DOM and API Word guessing game
 
-
+A fun Crystal guessing Game! 
 Tools used : BootStrap , Web APIs, 
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,37 @@
+// Crystal Words Arrays 
+var wordlist = ["Onyx", "Diamond", "Obsidain", "Quartz", "Jade", "Peridot"]
+//Empty Arrays to create blanks and letter 
+var lettersChosen = [];
+var blankWords= []; 
+
+// Queryselectors
+var win = document.querySelector(".wins");
+var lost = document.querySelector(".losses");
+var startBtn = document.querySelector(".Start");
+var WordGuess = document.querySelector("word-guess");
+var timer = document.querySelector(".timer-count");
+
+var winCounts = 0;
+var lostCounts = 0;
+var timer;
+var timerCounter;
+var wordChoice = "";
+var isWinner = false;
+
+    //renderword blanks 
+function startGame(){
+
+}
+function reset() {
+
+}
+
+
 // Time interval
-// .addeventlistner 
+// .addeventlistner to list for key events 
+//check letters to see if the letter the user press will populate 
+// 
+// click listner on start button and reset button
 //.localstorage.getitem
 // .localstorage.setitem
 // reset button and win and lose count 
