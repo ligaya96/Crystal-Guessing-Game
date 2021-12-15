af6d3e06da612d084d88e5bb7839070a9f7999c9
Ligaya <ligaya96@gmail.com>
2021-12-14 19:11:32 -0800

    adding majority of JS

--- assets/script.js
+++ assets/script.js
@@ -9,29 +9,135 @@ var win = document.querySelector(".wins");
 var lost = document.querySelector(".losses");
 var startBtn = document.querySelector(".Start");
 var WordGuess = document.querySelector("word-guess");
-var timer = document.querySelector(".timer-count");
-
+var timerElm = document.querySelector(".timer-count");
+var numBlanks = 0;
 var winCounts = 0;
 var lostCounts = 0;
 var timer;
 var timerCounter;
 var wordChoice = "";
 var isWinner = false;
+// function that is called when page is loading 
+function beginGame(){
+    getWins();
+    getLost();
+}
 
     //renderword blanks 
 function startGame(){
+    isWinner = false;
+    timerCounter = 10;
+    startBtn.disabled = true;
+    // begin timer and render the blanks 
+    renderBlanks();
+    timer();
 
 }
+// function for winning the game 
+function Winner(){
+    WordGuess.textcontent = "You Win!! Yay";
+    winCounts ++
+    startBtn.disabled = false;
+    setWins()
+}
+// function for Losing the game
+function Loser(){
+    WordGuess.textcontent = "You Lose! ";
+    lostCounts ++
+    startBtn.disabled = false;
+    setLost()
+}
+
+// Time interval set to start and triggers 
+function startTimer() {
+    // set timer
+    timer = setInterval(function(){
+        timerCounter--;
+        timerElm.textContent = timerCounter;
+        if (timerCounter >= 0 ) {
+            if (isWinner && timerCounter > 0 ){
+                clearInterval(timer);
+                Winner();
+            }
+        }
+        if (timerCounter === 0 ) {
+            clearInterval(timer);
+            Loser();
+        }
+    }, 1000);
+}
+// renders blanks 
+function renderBlanks(){
+    wordChoice = wordlist[Math.floor(Math.random() * words.length)];
+    lettersChosen = wordChoice.split("");
+    numBlanks = lettersChosen.length;
+    blankWords = []
+    // Using a loop to help render the blanks
+    for ( var i = 0; i < numBlanks; i++){
+        blankWords.push("_");
+    }
+    WordGuess.textContent = blankWords.join(" ")  
+}
+//.localstorage.getitem for wins and displaying win counts 
+// .localstorage.setitem for lost
+function getWins(){
+    var winScored = localStorage.getItem("winCounts");
+    if (winScored === null){
+        winCounts = 0;
+    }else {
+        winCounts = winScored;
+    }
+    win.textContent = winCounts;
+}
+ function setWins() {
+     win.textContent = winCounts;
+     localStorage.setItem("winCounts", winCounts);
+ }
+ function setLost() {
+    lost.textContent = lostCounts;
+    localStorage.setItem("lostCounts", lostCounts);
+}
+function getLost () {
+    var lostScored = localStorage.getItem("lostCounts");
+    if (lostScored === null){
+        lostCounts = 0;
+    } else {
+    lostCounts = lostScored;
+    }
+    lose.textContent = lostCounts;
+}
+// function to check letter and check if win
+function checkWords(letters) {
+    var letterinWords = false;
+    for (var i = 0 ; i < numBlanks; i++) {
+        if ( wordChoice[i] === letters) {
+            letterinWords = true;
+        }
+    }
+    if (letterinWords){
+        for ( var j = 0 , numBlanks; j++){
+            if(wordChoice[j] === letters) {
+            blankWords[j] = letters;
+            }
+        }
+        WordGuess.textContent = blankWords.join (" ");
+    }
+}
+//check letters to see if the letter the user press will populate 
+function checkwins(){
+    if (wordChoice === blankWords.join(" ")) {
+        isWinner = true;
+    }
+}
+// .addeventlistner to list for key events 
+
+// click listner on start button and reset button
+// reset button and win and lose count 
 function reset() {
 
 }
 
 
-// Time interval
-// .addeventlistner to list for key events 
-//check letters to see if the letter the user press will populate 
-// 
-// click listner on start button and reset button
-//.localstorage.getitem
-// .localstorage.setitem
-// reset button and win and lose count 
+
+
+
